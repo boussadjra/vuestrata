0b5cf20aaff096f6e6936bf6ed7927b6f3608008
boussadjra <brahim.boussadjra@gmail.com>
2026-05-02 23:42:05 +0100

    refactor(forms): move base-field composables to composables/forms/
    All useBase* composables are relocated from ui/base/ to composables/forms/
    where they belong per module-organization conventions. The ui/base/index.ts
    barrel becomes a thin re-export shim for backward compatibility.

---
+++ src/modules/app/composables/forms/index.ts
@@ -0,0 +1,39 @@
+export { useBaseTextField, type TextFieldProps } from './useBaseTextField'
+export { useBaseTextarea, type TextareaProps } from './useBaseTextarea'
+export { useBaseCheckbox, type CheckboxProps } from './useBaseCheckbox'
+export { useBaseSwitch, type SwitchProps } from './useBaseSwitch'
+export { useBaseRadioGroup, type RadioGroupProps, type RadioOption } from './useBaseRadioGroup'
+export { useBaseSearchField, type SearchFieldProps } from './useBaseSearchField'
+export { useBaseNumberField, type NumberFieldProps } from './useBaseNumberField'
+export { useBaseToggle, type ToggleProps } from './useBaseToggle'
+export {
+  useBaseToggleGroup,
+  type ToggleGroupProps,
+  type ToggleGroupOption,
+} from './useBaseToggleGroup'
+export {
+  useBaseSelect,
+  useBaseOption,
+  useBaseOptionGroup,
+  type SelectProps,
+  type SelectOption,
+  type SelectOptionGroup,
+} from './useBaseSelect'
+export { useBaseComboBox, type ComboBoxProps, type ComboBoxOption } from './useBaseComboBox'
+export { useBaseFileUpload, type FileUploadProps } from './useBaseFileUpload'
+export { useBaseOTPField, type OTPFieldProps } from './useBaseOTPField'
+export { useBaseTagsField, type TagsFieldProps } from './useBaseTagsField'
+export { useBaseTreeSelect, type TreeSelectProps, type TreeNode } from './useBaseTreeSelect'
+export { useBaseDateField, type DateFieldProps } from './useBaseDateField'
+export { useBaseTimeField, type TimeFieldProps } from './useBaseTimeField'
+export { useBaseCalendar, type CalendarProps } from './useBaseCalendar'
+export { useBaseSlider, type SliderProps } from './useBaseSlider'
+export { useBaseColorPicker, type ColorPickerProps } from './useBaseColorPicker'
+export { useBaseEditable, type EditableProps } from './useBaseEditable'
+export {
+  useBaseMentionsField,
+  type MentionsFieldProps,
+  type MentionItem,
+} from './useBaseMentionsField'
+export { useBaseRatingField, type RatingFieldProps } from './useBaseRatingField'
+export { useBaseSteppedForm, type SteppedFormProps } from './useBaseSteppedForm'
